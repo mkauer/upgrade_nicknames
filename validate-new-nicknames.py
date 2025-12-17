fbd22784d95ccc8fc841c3e41188399234b75bd8
mkauer <mkauer@icecube.wisc.edu>
2025-12-17 20:46:47 +1300

    add validator

---
+++ validate-new-nicknames.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+
+newnames = 'upgrade_devices.json'
+with open(newnames, 'r') as jfile:
+    upgrade = json.load(jfile)
+
+nicknames = 'gen1-nicknames.txt'
+with open(nicknames, 'r') as tfile:
+    gen1 = tfile.readlines()
+
+
+allnames = []
+for name in gen1:
+    name = name.strip()
+    if name not in allnames:
+        allnames.append(name)
+    else:
+        print(f'found again : {name}')
+
+for key in upgrade['devices']:
+    if "name" in key:
+        name = key['name'].strip()
+        if name not in allnames:
+            allnames.append(name)
+        else:
+            for key2 in upgrade['devices']:
+                if "name" in key2:
+                    if name == key2['name'].strip():
+                        pid2 = key2['prod_id'].strip()
+                        break
+            pid = key['prod_id'].strip()
+            print(f'found \"{name}\" in [{pid2}] and [{pid}]')
+
